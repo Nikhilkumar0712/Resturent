7e2be0b2f33fb9918614e03e113f82a8dbb65561
Nikhil kumar <102371163+Nikhilkumar0712@users.noreply.github.com>
2023-03-03 17:37:57 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <ul className="nav">
+            <ul className="nav" >
                 <li className="nav-item">
                     <a className="text-warning" href="#/"><h3>Foodify</h3></a>
                 </li>
             </ul>
             <center>
-                <h3 className="text-warning">Order online</h3>
+                <h3  style={{"background":"red"}}>Order online</h3>
             </center>
         </>
     )
